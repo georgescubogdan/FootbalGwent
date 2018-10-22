48b181b725959bef748b4f72e791daf49f5d9e28
Bogdan Georgescu <georgescubogdan96@gmail.com>
2018-10-22 12:12:04 +0300

    initial commit

---
+++ MPS/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace MPS
+{
+    public class GameManager
+    {
+
+        private static GameManager instance;
+        private static readonly object padLock = new object();
+        public ConcurrentDictionary<string, Snake> Snakes { get; set; }
+        public Timer Timer;
+        private SnakeHandler _snakeHandler;
+        public static GameManager Instance
+        {
+            get
+            {
+                lock (padLock)
+                {
+                    return instance ?? (instance = new GameManager());
+                }
+            }
+        }
+
+        public void Initilize(SnakeHandler snakeHandler)
+        {
+            _snakeHandler = snakeHandler;
+            Snakes = new ConcurrentDictionary<string, Snake>();
+            Timer = new Timer(Callback, null, 0, 1000 / 15);
+        }
+
+        private void Callback(object state)
+        {
+            var listOfSnakes = JsonConvert.SerializeObject(Snakes.Values);
+            //var a = Startup.ServiceProvider;
+            //var b = a.GetService<SnakeHandler>();
+            //var c = b.InvokeClientMethodToAllAsync("pingSnakes", listOfSnakes);
+            //c.Wait();
+            _snakeHandler.InvokeClientMethodToAllAsync("pingSnakes", listOfSnakes).Wait();
+            //Startup.ServiceProvider.GetRequiredService<SnakeHandler>()
+            //    .InvokeClientMethodToAllAsync("pingSnakes", listOfSnakes)
+            //    .Wait();
+        }
+
+    }
+}
---
+++ MPS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MPS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseUrls("https://localhost:5001", "https://192.168.1.132:5001", "http://localhost:5000", "http://localhost:5002", "http://192.168.1.132:5000", "http://192.168.1.132:5002")
+            .UseIISIntegration()
+            .UseStartup<Startup>();
+    }
+}
