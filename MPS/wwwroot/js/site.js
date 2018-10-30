b02e230faf16f98a53ae76aec2ea892003bc5929
denymar <37516054+denymar@users.noreply.github.com>
2018-10-30 21:38:28 +0200

    am șters un div inutil

--- MPS/wwwroot/js/site.js
+++ MPS/wwwroot/js/site.js
@@ -2,14 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var players = [];
+/*var players = [];
 var connection;
 var playersToSelect = 11;
 var functionalitiesToSelect = 5;
 var leadersToSelect = 1;
 var nextLevel = 0;
-var cardToPut = null;
-var receivedPlayers = [
+var cardToPut = null;*/
+let receivedPlayers = [
     {
         image: "https://placehold.it/200x200",
         name: "Player Name",
@@ -211,7 +211,7 @@ var receivedPlayers = [
         defense: 4
     }
 ]
-var receivedFunctionalities = [
+let receivedFunctionalities = [
     {
         image: "https://placehold.it/200x200",
         name: "Functionality name",
@@ -263,7 +263,7 @@ var receivedFunctionalities = [
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil perspiciatis ipsum quos non delectus iure vitae cumque inventore autem assumenda, aperiam magnam architecto suscipit blanditiis facere modi aut id ea."
     }
 ]
-var receivedLeaders = [
+let receivedLeaders = [
     {
         image: "https://placehold.it/200x200",
         name: "Leader Name",
@@ -300,11 +300,267 @@ window.onload = function () {
     //canv = document.getElementById("gc");
     //ctx = canv.getContext("2d");
     //document.addEventListener("keydown", keyPush);
-    var g = new game();
-    g.init;
+/*    var g = new game();
+    g.init;*/
     //map = new Map();
     //snake = new Snake();
     //apple = new Apple(15, 15);
+
+    const game = {
+        playersToSelect: 11,
+        functionalitiesToSelect: 5,
+        leadersToSelect: 1,
+        nextLevel: 0,
+        cardToPut: null,
+        init() {
+            this.modal = document.querySelector(".modal");
+            this.startBtn = document.querySelector("#startButton");
+            this.modalTitle = document.querySelector(".modal-title");
+            this.infoBox = document.querySelector(".info-box");
+            this.clickedCard = document.querySelector(".me .clicked-card");
+            this.startBtn.onclick = () => this.start();
+            this.nextBtn = document.querySelector("#nextButton");
+            this.nextBtn.onclick = () => this.next();
+            this.modalContent = document.querySelector(".select-items");
+            this.generatePlayers(receivedPlayers);
+            const generatedPlayers = document.querySelectorAll(".select-items .player");
+
+            this.asignClicks(generatedPlayers, "selected-player", "players");
+
+            this.myAttackLine = document.querySelector(".field .me .attack-line");
+            this.myMiddleLine = document.querySelector(".field .me .middle-line");
+            this.myDefenseLine = document.querySelector(".field .me .defense-line");
+            this.myGoalkeeperLine = document.querySelector(".field .me .goalkeeper-line");
+
+            this.myAttackLineScore = document.querySelector(".field .me .attack-line .line-score");
+            this.myMiddleLineScore = document.querySelector(".field .me .middle-line .line-score");
+            this.myDefenseLineScore = document.querySelector(".field .me .defense-line .line-score");
+            this.myGoalkeeperLineScore = document.querySelector(".field .me .goalkeeper-line .line-score");
+
+            this.myAttackLine.onclick = () => this.putClickedCardOnField(this.myAttackLine);
+            this.myMiddleLine.onclick = () => this.putClickedCardOnField(this.myMiddleLine);
+            this.myDefenseLine.onclick = () => this.putClickedCardOnField(this.myDefenseLine);
+            this.myGoalkeeperLine.onclick = () => this.putClickedCardOnField(this.myGoalkeeperLine);
+
+            console.log(this.myAttackLine);
+            console.log(this.myMiddleLine);
+            console.log(this.myDefenseLine);
+            console.log(this.myGoalkeeperLine);
+
+            /*
+                window.onclick = (e) => {
+                  if (e.target == this.modal) {
+                    this.modal.classList.remove("modal-active");
+                  }
+                }*/
+        },
+
+        asignClicks(arr, activeItemClass, itemsName) {
+            arr.forEach(item => {
+                item.onclick = () => {
+                    if (item.classList.contains(activeItemClass)) {
+                        item.classList.remove(activeItemClass);
+                        this[`${itemsName}ToSelect`]++;
+                        this.infoBox.innerHTML = `${itemsName} to select:<br>${this[`${itemsName}ToSelect`]}`;
+                    } else {
+                        if (this[`${itemsName}ToSelect`] > 0) {
+                            item.classList.add(activeItemClass);
+                            this[`${itemsName}ToSelect`]--;
+                            this.infoBox.innerHTML = `${itemsName} to select:<br>${this[`${itemsName}ToSelect`]}`;
+                        } else {
+                            alert(`You have already selected all ${itemsName}. Press "Next" to continue.`);
+                        }
+                    }
+                }
+            })
+        },
+
+        start() {
+            this.startBtn.style.display = "none";
+            this.modal.classList.add("modal-active");
+            this.infoBox.innerHTML = `Players to select:<br>11`;
+        },
+
+        next() {
+            if (this.nextLevel === 0) {
+                if (this.playersToSelect > 0) {
+                    alert(`Please, select ${this.playersToSelect} more players`);
+                } else {
+                    this.selectedPlayers = document.querySelectorAll(".selected-player");
+                    this.nextLevel++;
+                    this.modalTitle.innerHTML = "Select functionalities";
+                    this.infoBox.innerHTML = `Functionalities to select:<br>5`;
+                    this.generateFunctionalities(receivedFunctionalities);
+                    const generatedFunctionalities = document.querySelectorAll(".select-items .functionality");
+                    this.asignClicks(generatedFunctionalities, "selected-functionality", "functionalities");
+                }
+            } else if (this.nextLevel === 1) {
+                if (this.functionalitiesToSelect > 0) {
+                    alert(`Please, select ${this.functionalitiesToSelect} more functionalities`);
+                } else {
+                    this.selectedFunctionalities = document.querySelectorAll(".selected-functionality");
+                    this.nextLevel++;
+                    this.nextBtn.innerHTML = "Start";
+                    this.modalTitle.innerHTML = "Select leader";
+                    this.infoBox.innerHTML = `Leader to select:<br>1`;
+                    this.generateLeaders(receivedLeaders);
+                    const generatedLeaders = document.querySelectorAll(".select-items .leader");
+                    this.asignClicks(generatedLeaders, "selected-leader", "leaders");
+                }
+            } else if (this.nextLevel === 2) {
+                this.selectedLeader = document.querySelector(".selected-leader");
+                this.modal.classList.remove("modal-active");
+                this.nextBtn.innerHTML = "Next";
+                console.log(this.selectedPlayers);
+                console.log(this.selectedFunctionalities);
+                console.log(this.selectedLeader);
+                this.putMySelectedCards();
+            }
+        },
+
+        putClickedCardOnField(line) {
+            cardsPlace = line.querySelector(".cards-place");
+            score = line.querySelector(".line-score");
+            let newScore = 0;
+            if (this.cardToPut) {
+                cardsPlace.appendChild(this.cardToPut);
+                this.cardToPut.onclick = () => this.showClickedCard(this.cardToPut);
+                this.cardToPut = null;
+                this.clickedCard.innerHTML = "";
+
+                const players = line.querySelectorAll(".player");
+                players.forEach(item => {
+                    item.onclick = () => this.showClickedCard(item);
+                    console.log(item.querySelector(".attack"));
+                    console.log(item.querySelector(".defense"));
+                    console.log(item.querySelector(".attack span"));
+                    console.log(item.querySelector(".defense span"));
+                    att = Number(item.querySelector(".attack span").innerHTML);
+                    def = Number(item.querySelector(".defense span").innerHTML);
+                    newScore += att + def;
+                });
+                score.innerHTML = newScore;
+
+                const functionalities = line.querySelectorAll(".functionality");
+                functionalities.forEach(item => item.onclick = () => this.showClickedCard(item));
+
+                if ((players.length + functionalities.length) > 8) {
+                    cardsPlace.style.justifyContent = "flex-start";
+                }
+            }
+        },
+
+        showClickedCard(item) {
+            this.clickedCard.innerHTML = "";
+            newItem = item.cloneNode(true);
+            this.clickedCard.appendChild(newItem);
+        },
+
+        putClickedCard(item) {
+            this.showClickedCard(item);
+            this.cardToPut = item;
+        },
+
+        putMySelectedCards() {
+            this.leaderPlace = document.querySelector(".info .me .leader-place");
+            this.cardsPlace = document.querySelector(".info .me .cards-place");
+
+            this.selectedLeader.classList.remove("selected-leader");
+            this.leaderPlace.appendChild(this.selectedLeader);
+
+            this.selectedFunctionalities.forEach(item => {
+                item.onclick = () => this.putClickedCard(item);
+                item.classList.remove("selected-functionality");
+                this.cardsPlace.appendChild(item);
+            });
+            this.selectedPlayers.forEach(item => {
+                item.onclick = () => this.putClickedCard(item);
+                item.classList.remove("selected-player");
+                this.cardsPlace.appendChild(item);
+            });
+        },
+
+        generateLeaders(arr) {
+            const { modalContent: leaders } = this;
+            leaders.innerHTML = "";
+            arr.forEach(item => {
+                leaders.innerHTML += `
+      <div class="leader">
+        <div class="photo">
+          <img src=${item.image} alt="leader photo">
+        </div>
+        <div class="information">
+          <div class="name">
+            ${item.name}
+          </div>
+          <div class="description">
+            ${item.description}
+          </div>
+        </div>
+      </div>
+      `
+            })
+        },
+
+        generateFunctionalities(arr) {
+            const { modalContent: functionalities } = this;
+            functionalities.innerHTML = "";
+            arr.forEach(item => {
+                functionalities.innerHTML += `
+      <div class="functionality">
+        <div class="photo">
+          <img src=${item.image} alt="functionality photo">
+        </div>
+        <div class="information">
+          <div class="name">
+            ${item.name}
+          </div>
+          <div class="description">
+            ${item.description}
+          </div>
+        </div>
+
+      </div>
+      `
+            })
+        },
+
+        generatePlayers(arr) {
+            const { modalContent: players } = this;
+            players.innerHTML = "";
+            arr.forEach(item => {
+                players.innerHTML += `
+      <div class="player">
+        <div class="photo">
+          <img src=${item.image} alt="player photo">
+        </div>
+        <div class="information">
+          <div class="name">
+            ${item.name}
+          </div>
+          <div class="country">
+            ${item.country}
+          </div>
+          <div class="fc">
+            ${item.fc}
+          </div>
+          <div class="stats">
+            <div class="attack">
+              <span>${item.attack}</span>
+            </div>
+            <div class="defense">
+              <span>${item.defense}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+            })
+        }
+    }
+
+
+    game.init();
 
     connection = new WebSocketManager.Connection("ws://localhost:5000/server");
 
@@ -342,7 +598,7 @@ function Player() {
 }
 
 
-function game() {
+/*function game() {
     this.modal = document.querySelector(".modal");
     this.startBtn = document.querySelector("#startButton");
     this.modalTitle = document.querySelector(".modal-title");
@@ -380,8 +636,9 @@ function game() {
             this.modal.classList.remove("modal-active");
         }
     }
-}
+}*/
 
+/*
 game.prototype.init = function () {
    
     console.log(this.myAttackLine);
@@ -395,8 +652,9 @@ game.prototype.init = function () {
         this.modal.classList.remove("modal-active");
       }
     }
-}
+}*/
 
+/*
 game.prototype.asignClicks = function (arr, activeItemClass, itemsName) {
     arr.forEach(item => {
         item.onclick = () => {
@@ -415,14 +673,16 @@ game.prototype.asignClicks = function (arr, activeItemClass, itemsName) {
             }
         }
     })
-}
+}*/
 
+/*
 game.prototype.start = function () {
     this.startBtn.style.display = "none";
     this.modal.classList.add("modal-active");
     this.infoBox.innerHTML = `Players to select:<br>11`;
-}
+}*/
 
+/*
 game.prototype.next = function () {
     if (this.nextLevel === 0) {
         if (this.playersToSelect > 0) {
@@ -458,8 +718,9 @@ game.prototype.next = function () {
         console.log(this.selectedLeader);
         this.putMySelectedCards();
     }
-}
+}*/
 
+/*
 game.prototype.putClickedCardOnField = function (line) {
     cardsPlace = line.querySelector(".cards-place");
     score = line.querySelector(".line-score");
@@ -490,19 +751,22 @@ game.prototype.putClickedCardOnField = function (line) {
             cardsPlace.style.justifyContent = "flex-start";
         }
     }
-}
+}*/
 
+/*
 game.prototype.showClickedCard = function (item) {
     this.clickedCard.innerHTML = "";
     newItem = item.cloneNode(true);
     this.clickedCard.appendChild(newItem);
-}
+}*/
 
+/*
 game.prototype.putClickedCard = function (item) {
     this.showClickedCard(item);
     this.cardToPut = item;
-}
+}*/
 
+/*
 game.prototype.putMySelectedCards = function () {
     this.leaderPlace = document.querySelector(".info .me .leader-place");
     this.cardsPlace = document.querySelector(".info .me .cards-place");
@@ -520,8 +784,9 @@ game.prototype.putMySelectedCards = function () {
         item.classList.remove("selected-player");
         this.cardsPlace.appendChild(item);
     });
-}
+}*/
 
+/*
 game.prototype.generateLeaders = function (arr) {
     const { modalContent: leaders } = this;
     leaders.innerHTML = "";
@@ -542,8 +807,9 @@ game.prototype.generateLeaders = function (arr) {
       </div>
       `
     })
-}
+}*/
 
+/*
 game.prototype.generateFunctionalities = function (arr) {
     const { modalContent: functionalities } = this;
     functionalities.innerHTML = "";
@@ -565,8 +831,9 @@ game.prototype.generateFunctionalities = function (arr) {
       </div>
       `
     })
-}
+}*/
 
+/*
 game.prototype.generatePlayers = function (arr) {
     const { modalContent: players } = this;
     players.innerHTML = "";
@@ -598,7 +865,7 @@ game.prototype.generatePlayers = function (arr) {
       </div>
       `;
     })
-}
+}*/
 
 
 function keyPush(evt) {
